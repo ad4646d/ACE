6de7f4017f66adae9b032f66e00c5d9b106e749a
ad4646d <ad4646d@gre.ac.uk>
2021-10-12 17:21:17 +0100

    <feat> Added CPU usage command

--- systemStats.sh
+++ systemStats.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/
 # CREATED BY: Andrew Dean
-# Date created: 5/Oct/2021
+# Date created: 12/Oct/2021
 # Version: V0.2
 
 # This script is designed to gather system statistics
 AUTHOR="Andrew Dean"
-VERSION="V0.1"
-RELEASED="2021-10-05"
+VERSION="V0.2"
+RELEASED="2021-10-12"
 
 # Display a help message
 
 USAGE(){
 	echo -e $1
-	echo -e "\nUsage: systemStats [-t temperature] [-i ipv4 address]"
+	echo -e "\nUsage: systemStats [-t temperature] [-i ipv4 address] [-c cpu usage]"
 	echo -e "\t\t [-v version]"
 	echo -e "\t\t For more information - see man systemStats"
 
@@ -34,12 +34,17 @@ fi
 # With the flags method, some of the arguments can be made mandatory or optional
 # EXAMPLE - a:b (a is mandatory, whereas b is optional) abc is all optional
 
-while getopts tiv OPTION
+while getopts ctiv OPTION
 do
 case ${OPTION}
 in
 i) IP=$(ifconfig wlan0 | grep -w inet | awk '{print$2}')
    echo ${IP};;
+c) USAGE=$(grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
+					   {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)}
+					    END {printf "Used CPU: %.2f%%\n",usage}
+						{printf "Free CPU: %.2f%%\n", free}')
+   echo ${USAGE};;
 t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
    echo ${TEMP} "Need to divide by 1000 to get degC.";;
 v) echo -e "systemStats:\n\t Version: ${VERSION} Released on: ${RLEASED} Created by: ${AUTHOR}";;
