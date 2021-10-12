f275d111bb6588563a5c5a778f7622d4905097f7
ad4646d <ad4646d@gre.ac.uk>
2021-10-12 19:36:08 +0100

    <feat> Added logging functionality

--- systemStats.sh
+++ systemStats.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/
 # CREATED BY: Andrew Dean
-# Date created: 12/Oct/2021
-# Version: V0.2
+# Date created: 19/Oct/2021
+# Version: V0.3
 
 # This script is designed to gather system statistics
 AUTHOR="Andrew Dean"
-VERSION="V0.2"
-RELEASED="2021-10-12"
-
+VERSION="V0.3"
+RELEASED="2021-10-19"
+FILE=~/ACE/systemstats.log
 # Display a help message
 
 USAGE(){
@@ -42,8 +42,8 @@ i) IP=$(ifconfig wlan0 | grep -w inet | awk '{print$2}')
    echo ${IP};;
 c) USAGE=$(grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
 					   {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)}
-					    END {printf "Used CPU: %.2f%%\n",usage}
-						{printf "Free CPU: %.2f%%\n", free}')
+					    END {printf " Used CPU: %.2f%%",usage}
+						{printf "Free CPU: %.2f%%", free}')
    echo ${USAGE};;
 t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
    echo ${TEMP} "Need to divide by 1000 to get degC.";;
@@ -51,5 +51,7 @@ v) echo -e "systemStats:\n\t Version: ${VERSION} Released on: ${RLEASED} Created
 esac
 done
 
+NOW=$(date +%FT%R':'%SZ)
+echo -e ${NOW} "\tIP: ${IP} Temperature: ${TEMP} ${USAGE}" >> ${FILE}
 
 # End of script
